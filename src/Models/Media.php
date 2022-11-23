009849eca926d5c7b75528e63722743cdeee4130
Jonathan Orta <emanuelorta93@hotmail.com>
2022-11-23 15:24:10 -0600

    first commit

---
+++ src/Models/Media.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Markerly\Scrapper\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Media extends Model
+{
+    protected $fillable = [
+        'post_id',
+        'reel_id',
+        'is_video',
+        'video_duration',
+        'url'
+    ];
+
+    protected $casts = [
+        'is_video' => 'boolean'
+    ];
+}
